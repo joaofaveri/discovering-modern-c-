d9a2db3edbf16809c1ca1f54f8f962cb394682d8
joaofaveri <joao.faveri@gmail.com>
2022-06-10 19:01:42 -0300

    Hello 42 in C++

---
+++ hello42.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "The answer to the Universe Question of Life,\n"
+              << "the Universe, and Everything is:"
+              << std::endl
+              << 6 * 7
+              << std::endl;
+    return 0;
+}
