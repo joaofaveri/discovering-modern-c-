524b03ebd90590dbcaf51fc1e14af27c0493fc06
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-06-14 15:21:08 -0300

    Chapter 1 - Part 2 - Item 2 - Characters and Strings

--- Chapter 1/strings.cpp
+++ Chapter 1/strings.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
+    // C++ strings use dynamic memory
     string name = "João Paulo";
+    /* So if we append more text to a string
+     * we don't neeed to worry about memory
+     * corruption or cutting off strings
+     */
+    name = name + " de Faveri";
+    cout << name << endl;
 }
